56e07430c5fd68a5f6298d94ffdc2c59c253632e
Mohammad Saddam Hossain <sddmhossain786@gmail.com>
2026-02-13 09:07:16 +0530

    initial commit

---
+++ src/test/java/com/urjaslot/UrjaslotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urjaslot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrjaslotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
